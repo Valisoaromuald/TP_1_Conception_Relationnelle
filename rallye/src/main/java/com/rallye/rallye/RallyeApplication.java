795233128bbb3415cb2f907ae6b143ac26684fb3
Valisoaromuald <romualdrabenandrasana@gmail.com>
2025-11-04 05:11:42 +0300

    feat:projet initilialise avec security config

---
+++ rallye/src/main/java/com/rallye/rallye/RallyeApplication.java
@@ -0,0 +1,13 @@
+package com.rallye.rallye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RallyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RallyeApplication.class, args);
+	}
+
+}
---
+++ rallye/src/test/java/com/rallye/rallye/RallyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rallye.rallye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RallyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
